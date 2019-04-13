2308b2af6123486879c3715a96b80444bc0d9eff
João Paulo Queiroz <jaoopaulo.queiroz.545@gmail.com>
2019-04-13 12:21:49 -0300

    server

--- server-auth/routes/users.js
+++ server-auth/routes/users.js
@@ -1,6 +1,18 @@
 var express = require("express");
 var router = express.Router();
 var cors = require("cors");
+var jwt = require("../services/utils.js");
+var i  = 'Mysoft corp';          // Issuer 
+var s  = 'some@user.com';        // Subject 
+var a  = 'http://mysoftcorp.in'; // Audience
+
+var signOptions = {
+      issuer:  i,
+      subject:  s,
+      audience: a,
+      expiresIn:  120,    // 30 days validity
+      algorithm:  "RS256"    
+  };
 
 router.all("http://localhost:3000", cors());
 
@@ -11,6 +23,14 @@ router.get("/", function(req, res, next) {
 
 router.post("/create", (req, res, next) => {
   console.log(req.body);
+  return res.send({ok: 'ok'})
+});
+
+router.post("/login", (req, res, next) => {
+  console.log(req.body);
+  let token = jwt.sign({data1: req.body.password}, signOptions);
+  console.log(token)
+  return res.send({token: token})
 });
 
 module.exports = router;
