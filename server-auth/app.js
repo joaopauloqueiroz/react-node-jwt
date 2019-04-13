6596bf5c074beee855099d9d07a5d86776443497
João Paulo Queiroz <jaoopaulo.queiroz.545@gmail.com>
2019-04-13 14:51:55 -0300

    token

--- server-auth/app.js
+++ server-auth/app.js
@@ -6,6 +6,7 @@ var logger = require("morgan");
 var cors = require("cors");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var passport = require('passport')
 
 var app = express();
 
@@ -19,6 +20,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+passport.use(strategy);
+app.use(passport.initialize());
+app.use(parser.urlencoded({
+  extended: false
+}));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
--- server-auth/services/utils.js
+++ server-auth/services/utils.js
@@ -50,3 +50,26 @@ verify: (token, $Option) => {
     //returns null if token is invalid
  }
 }
+
+/*
+  const jwt = require('jsonwebtoken')
+const passportJWT = require('passport-jwt');
+const JwtStrategy = passportJWT.Strategy;
+const ExtractJwt = passportJWT.ExtractJwt; 
+
+const secret = `
+  MIIBOwIBAAJBAIwwEvcnfiUeAoMlxBQF/It4qCYqHNdTz2HNvnkmkUqIZc/5xKo8kLrSJok5Mdt5J3RSrSeYtWMGb0KMJ+Jm1A8CAwEAAQJAQBamRPl4EO30RhKY3nKSLwzdCn2MK+Cz2Ume/3HjUEf3uNXMC7/sxgfikf+ol7aJKY5BZRh1gvh+WCG6nKXGaQIhAMRRx+SlnBPg5LtYwaFnwgowFe7p6GYJjpJOlxIncLO1AiEAts3xt0PUlCTQVjsV+SB1CF22llgO3+jCKbGb8WM5SzMCIBMH3nnCuBORPHsQyoxr83prWOT1Fcs2Z/S0254t5pX1AiEAjWnflVAJwNiBulNW+jPblS85sjBIwXoDydE1u5Rwm/UCIQC5HQkIMgrTsottm8ppMK5J4ytaXwkLdx69jClf+oJMcA==
+  `;
+
+const opts = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: secret
+};
+
+const payload = { id: 10};
+const token = jwt.sign(payload, secret);
+console.log(token);
+
+
+  
+*/
